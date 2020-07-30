2bd1292c96433db5f651ce9c0726c23501a3f378
Alexander Lobanov <anklav24@gmail.com>
2020-07-31 03:18:41 +0700

    Edit for pyowm~=2.10.0 version on Heroku.

--- WeatherTelegramBot.py
+++ WeatherTelegramBot.py
@@ -5,7 +5,6 @@
 #              https://github.com/csparpa/pyowm
 
 import pyowm  # Импортируем пакет с помощью которого мы узнаем погоду
-# from pyowm.utils.config import get_default_config  # Импорт конфига owm
 import telebot  # Импортируем пакет бота через ввод в CMD "pip install pytelegrambotapi"
 import os  # Импортируем для использования переменных окружения
 import time
@@ -14,7 +13,6 @@ import time
 # config_dict['language'] = 'ru'  # Настраиваем язык для owm.
 owmToken = os.getenv('YOUR_OWN_TOKEN')  # Регистрируемся на сайте погоды, получаем ключ API
 owm = pyowm.OWM(owmToken, language='ru')
-mgr = owm.weather_manager()
 botToken = os.getenv('YOUR_TELEGRAM_BOT_TOKEN')  # Получаем токен через BotFather в телеграме в чате коммандами. /newbot имя моего APITelegramBot
 bot = telebot.TeleBot(botToken)
 
@@ -41,13 +39,13 @@ def send_message(message):
         # и выводит ошибку, то происходит переход к except
         try:
             # Имя города пользователь вводит в чат, после этого мы его передаем в функцию
-            observation = mgr.weather_at_place(message.text)
-            weather = observation.weather
-            temp = weather.temperature("celsius")["temp"]  # Присваиваем переменной значение температуры из таблицы
-            print(time.ctime(), "Message:", message.text, temp, "C", weather.detailed_status)
+            observation = owm.weather_at_place(message.text)
+            weather = observation.get_weather()
+            temp = weather.get_temperature("celsius")["temp"]  # Присваиваем переменной значение температуры из таблицы
+            print(time.ctime(), "Message:", message.text, temp, "C", weather.get_detailed_status())
 
             # Формируем и выводим ответ
-            answer = "В городе " + message.text + " сейчас " + weather.detailed_status + "." + "\n"
+            answer = "В городе " + message.text + " сейчас " + weather.get_detailed_status() + "." + "\n"
             answer += "Температура около: " + str(temp) + " С" + "\n\n"
             if temp < -10:
                 answer += "Пи**ц как холодно, одевайся как танк!"
