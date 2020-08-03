4819563449ca33d18281887605dd57906ca0b5db
Alexander Lobanov <anklav24@gmail.com>
2020-08-03 16:13:49 +0700

    Rename a mistype OWN to OWM.

--- tests/test_weather_telegram_bot.py
+++ tests/test_weather_telegram_bot.py
@@ -4,8 +4,9 @@ import unittest
 
 class TestTokensIsExists(unittest.TestCase):
     """Check is existing tokens"""
+
     def test_owm_token_is_exists(self):
-        self.assertNotEqual(os.getenv('YOUR_OWN_TOKEN', ""), "")
+        self.assertNotEqual(os.getenv('YOUR_OWM_TOKEN', ""), "")
 
     def test_telegram_bot_token_is_exists(self):
         self.assertNotEqual(os.getenv('YOUR_TELEGRAM_BOT_TOKEN', ""), "")
--- weather_telegram_bot.py
+++ weather_telegram_bot.py
@@ -11,7 +11,7 @@ import time
 
 # config_dict = get_default_config()
 # config_dict['language'] = 'ru'  # Настраиваем язык для owm.
-owmToken = os.getenv('YOUR_OWN_TOKEN')  # Регистрируемся на сайте погоды, получаем ключ API
+owmToken = os.getenv('YOUR_OWM_TOKEN')  # Регистрируемся на сайте погоды, получаем ключ API
 owm = pyowm.OWM(owmToken, language='ru')
 botToken = os.getenv('YOUR_TELEGRAM_BOT_TOKEN')  # Получаем токен через BotFather в телеграме в чате коммандами. /newbot имя моего APITelegramBot
 bot = telebot.TeleBot(botToken)
