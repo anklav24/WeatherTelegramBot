09c191651fe700b12c0750616ae0eb7c204e4015
Alexander Lobanov <anklav24@gmail.com>
2020-08-03 16:56:02 +0700

    Rewrite tests on pyTest

--- tests/test_weather_telegram_bot.py
+++ tests/test_weather_telegram_bot.py
@@ -1,16 +1,11 @@
 import os
-import unittest
 
 
-class TestTokensIsExists(unittest.TestCase):
+class TestTokensIsExists:
     """Check is existing tokens"""
 
     def test_owm_token_is_exists(self):
-        self.assertNotEqual(os.getenv('YOUR_OWM_TOKEN', ""), "")
+        assert os.getenv('YOUR_OWM_TOKEN', "") != ""
 
     def test_telegram_bot_token_is_exists(self):
-        self.assertNotEqual(os.getenv('YOUR_TELEGRAM_BOT_TOKEN', ""), "")
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert os.getenv('YOUR_TELEGRAM_BOT_TOKEN', "") != ""
